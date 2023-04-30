c575bad9f9a663e903ec4d56d1b42dc923a5c447
유지은 <wlwl0219@humaxparcs.com>
2023-05-01 05:35:58 +0900

    update layout flex-col

--- apps/frontend/components/layouts/Layout.tsx
+++ apps/frontend/components/layouts/Layout.tsx
@@ -5,9 +5,9 @@ import Sidebar from "./Sidebar";
 
 const Layout = ({ children }) => {
   return (
-    <div className="flex">
+    <div className="flex justify-center">
       <Sidebar />
-      <div className="flex-1">
+      <div className="flex-1 flex flex-col">
         <Header />
         <div className="w-full h-full bg-gray-900">{children}</div>
       </div>
