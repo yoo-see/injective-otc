526933d13a79914b38899fe4878d96ab9734ec8a
유지은 <wlwl0219@humaxparcs.com>
2023-05-02 21:50:52 +0900

    컬러시스템 추가

--- apps/frontend/tailwind.config.js
+++ apps/frontend/tailwind.config.js
@@ -4,11 +4,15 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        "omi-blue": "#2864ff;",
-        "omi-black": "#22252e;",
-        "grey/2": "#EAEAEA;",
-        "grey/9": "#22252e;",
-        "grey/10": "#191B23;",
+        "omi-blue": "#2864ff",
+        "omi-black": "#22252e",
+        "grey/1": "#FAFAFB",
+        "grey/2": "#EAEAEA",
+        "grey/3": "#E0E0E0",
+        "grey/4": "#C6C6C6",
+        "grey/5": "#8E8E93",
+        "grey/9": "#22252e",
+        "grey/10": "#191B23",
       },
       fontFamily: {
         sans: ["Pretendard", "Poppins"],
