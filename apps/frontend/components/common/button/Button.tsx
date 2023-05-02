1fe6507fedb5793be55aeed2f4f9dd8a3be73c6b
유지은 <wlwl0219@humaxparcs.com>
2023-05-02 21:51:19 +0900

    베이직 버튼 disabled props, 스타일 적용

--- apps/frontend/components/common/button/Button.tsx
+++ apps/frontend/components/common/button/Button.tsx
@@ -4,13 +4,22 @@ interface Props {
   className?: string;
   text?: string;
   onClick?: () => void;
+  disabled?: boolean;
 }
 
-const Button: React.FC<Props> = ({ className, text, onClick }) => {
+const Button: React.FC<Props> = ({
+  className,
+  text,
+  onClick,
+  disabled = false,
+}) => {
+  const disabledClassName = disabled ? "bg-grey/5 " : "bg-omi-blue";
   const mergedClassName = twMerge(
-    `bg-omi-blue w-[428px] h-14 rounded-lg Poppins text-white leading-6 font-normal text-base`,
+    `w-[428px] h-14 rounded-lg Poppins text-white leading-6 font-normal text-base`,
+    disabledClassName,
     className,
   );
+
   return (
     <button className={mergedClassName} onClick={onClick}>
       <span>{text ? text : "Go to history"}</span>
