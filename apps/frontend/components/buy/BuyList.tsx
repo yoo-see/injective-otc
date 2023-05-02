b8845bd8c0606ba5fea76c76a3513aafa66d1ba6
hunhunseunghun <hunhunseunghun@gmail.com>
2023-05-02 13:50:17 +0900

    scrollbar hide

--- apps/frontend/components/buy/BuyList.tsx
+++ apps/frontend/components/buy/BuyList.tsx
@@ -10,8 +10,8 @@ const BuyList = () => {
   );
 
   const tags = (
-    <tr className="h-[66px] ">
-      <td className="align-middle">
+    <tr className="h-[66px] w-full">
+      <td className="align-middle w-[170px]">
         <div className="flex items-center gap-2 ml-3 p-0">
           <div>1.</div>
           <EtherIcon />
@@ -26,14 +26,14 @@ const BuyList = () => {
           </div>
         </div>
       </td>
-      <td>
+      <td className="w-[170px]">
         <div className="flex flex-row items-center gap-1.5 p-0">
           {" "}
           <EtherIcon />
           <span>1519.00</span>
         </div>
       </td>
-      <td className="align-middle gap-2 p-0">
+      <td className="w-[170px] align-middle gap-2 p-0">
         <div className="flex items-center gap-2 p-0">
           <EtherIcon />
           <div className="flex flex-col items-start gap-0.5 h-9 p-0">
@@ -47,17 +47,17 @@ const BuyList = () => {
           </div>
         </div>
       </td>
-      <td className="mr-3">
+      <td className="w-[170px] mr-3">
         <span>2023.03.25 16:00</span>
       </td>
-      <td className="align-middle text-center p-0">
+      <td className="w-[170px] align-middle text-center p-0">
         <button className="bg-omi-blue w-[79px] h-8 rounded-[14px]">Buy</button>
       </td>
     </tr>
   );
 
   return (
-    <section className="">
+    <div className="px-6 ">
       <header>
         <div className="flex flex-row items-center gap-2 w-[214px] h-[42px] p-0">
           <h1 className={h1MergedClassName}>OTC Market</h1>
@@ -67,7 +67,7 @@ const BuyList = () => {
       </header>
       <body className="mt-5">
         <table className="block w-[884px] Pretendard text-[#FFFFFF] font-normal text-[15px] leading-[18px]">
-          <thead className="h-10 Pretendard text-[#FFFFFF]  font-normal text-[15px] leading-[18px] bg-[#22252E]">
+          <thead className="w-[884px] h-10 Pretendard text-[#FFFFFF]  font-normal text-[15px] leading-[18px] bg-[#22252E]">
             <tr className="text-left">
               <th className="pl-3 w-[170px] font-normal p-0">All</th>
               <th className="w-[170px] font-normal p-0">Price</th>
@@ -78,8 +78,8 @@ const BuyList = () => {
               </th>
             </tr>
           </thead>
-          <tbody className="block w-full max-h-[594px] font-normal overflow-y-scroll">
-            <tr className="h-[66px] w-ful">
+          <tbody className="block w-full max-h-[594px] font-normal overflow-y-scroll scrollbar-hide">
+            <tr className="h-[66px] w-full">
               <td className="align-middle w-[170px]">
                 <div className="flex items-center gap-2 ml-3 p-0">
                   <div>1.</div>
@@ -126,24 +126,13 @@ const BuyList = () => {
               </td>
             </tr>
 
-            {tags}
-            {tags}
-            {tags}
-            {tags}
-            {tags}
-            {tags}
-            {tags}
-            {tags}
-            {tags}
-            {tags}
-            {tags}
-            {tags}
-            {tags}
-            {tags}
+            {Array(100)
+              .fill(tags)
+              .map((ele) => ele)}
           </tbody>
         </table>
       </body>
-    </section>
+    </div>
   );
 };
 
