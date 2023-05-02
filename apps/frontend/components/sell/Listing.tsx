8ab57c7898b4fc06759cf5ce5b304b5f465e1dff
SeungHun <hunhunseunghun@gmail.com>
2023-05-02 22:56:06 +0900

    make a list to sell button completed

--- apps/frontend/components/sell/Listing.tsx
+++ apps/frontend/components/sell/Listing.tsx
@@ -20,76 +20,84 @@ const Listing = () => {
   };
 
   return (
-    <div className="bg-[#1F212C] w-[492px] h-[528px] px-8 py-11  rounded-[20px]">
+    <div className="bg-[#1F212C] w-[492px] px-8 py-11 rounded-[20px]">
       <div className="Poppins w-[198px] h-[42px] font-semibold text-white text-[28px] leading-[42px] tracking-[-0.24px]">
         Listing
       </div>
+
       <div className="mt-6">
-        <div className=" flex flex-col items-start p-0 gap-6">
-          <div>
-            <span className="font-normal text-[15px] leading-[18px] text-[#EAEAEA]">
-              Amount
-            </span>
-            <div className="bg-white flex flex-row justify-between items-center w-[428px] h-[52px] px-5 py-[15px] rounded-[10px] mt-2">
-              <div className="flex flex-row items-center gap-2 p-0 hover:cursor-pointer">
-                <div className="flex flex-row items-center gap-1.5 w-[61px] h-[27px] p-0">
-                  <EtherIcon />
-                  <div className="font-midium text-base leading-[27px] text-[#191B23]">
-                    ETH
+        <div>
+          <div className=" flex flex-col items-start p-0 gap-6">
+            <div>
+              <span className="font-normal text-[15px] leading-[18px] text-[#EAEAEA]">
+                Amount
+              </span>
+              <div className="bg-white flex flex-row justify-between items-center w-[428px] h-[52px] px-5 py-[15px] rounded-[10px] mt-2">
+                <div className="flex flex-row items-center gap-2 p-0 hover:cursor-pointer">
+                  <div className="flex flex-row items-center gap-1.5 w-[61px] h-[27px] p-0">
+                    <EtherIcon />
+                    <div className="font-midium text-base leading-[27px] text-[#191B23]">
+                      ETH
+                    </div>
                   </div>
+                  <Vector />
                 </div>
-                <Vector />
+                <input
+                  type="number"
+                  onChange={handleAmountValue}
+                  className={`Pretendard font-medium text-xl leading-6 text-[#191B23] text-right focus:outline-0 ${
+                    amountValue === 0 && "text-[#E0E0E0]"
+                  }`}
+                />
               </div>
-              <input
-                type="number"
-                onChange={handleAmountValue}
-                className={`Pretendard font-medium text-xl leading-6 text-[#191B23] text-right focus:outline-0 ${
-                  amountValue === 0 && "text-[#E0E0E0]"
-                }`}
-              />
             </div>
-          </div>
-          <div>
-            <span className="font-normal text-[15px] leading-[18px] text-[#EAEAEA]">
-              Price
-            </span>
-            <div className="bg-white flex flex-row justify-between items-center w-[428px] h-[52px] px-5 py-[15px] rounded-[10px] mt-2">
-              <div className="flex flex-row items-center gap-2 p-0 hover:cursor-pointer">
-                <UsdcIcon />
+            <div>
+              <span className="font-normal text-[15px] leading-[18px] text-[#EAEAEA]">
+                Price
+              </span>
+              <div className="bg-white flex flex-row justify-between items-center w-[428px] h-[52px] px-5 py-[15px] rounded-[10px] mt-2">
+                <div className="flex flex-row items-center gap-2 p-0 hover:cursor-pointer">
+                  <UsdcIcon />
+                </div>
+                <input
+                  type="number"
+                  onChange={handlePriceValue}
+                  className={`Pretendard font-medium text-xl leading-6 text-[#191B23] text-right focus:outline-0 ${
+                    priceValue === 0 && "text-[#E0E0E0]"
+                  }`}
+                  placeholder="0.00"
+                />
               </div>
-              <input
-                type="number"
-                onChange={handlePriceValue}
-                className={`Pretendard font-medium text-xl leading-6 text-[#191B23] text-right focus:outline-0 ${
-                  priceValue === 0 && "text-[#E0E0E0]"
-                }`}
-                placeholder="0.00"
-              />
             </div>
-          </div>
-          <div>
-            <span className="font-normal text-[15px] leading-[18px] text-[#EAEAEA]">
-              Collateral
-            </span>
-            <div className="bg-white flex flex-row justify-between items-center w-[428px] h-[52px] px-5 py-[15px] rounded-[10px] mt-2">
-              <div className="flex flex-row items-center gap-2 p-0 hover:cursor-pointer">
-                <div className="flex flex-row items-center gap-1.5 w-[61px] h-[27px] p-0">
-                  <EtherIcon />
-                  <div className="font-midium text-base leading-[27px] text-[#191B23]">
-                    ETH
+            <div>
+              <span className="font-normal text-[15px] leading-[18px] text-[#EAEAEA]">
+                Collateral
+              </span>
+              <div className="bg-white flex flex-row justify-between items-center w-[428px] h-[52px] px-5 py-[15px] rounded-[10px] mt-2">
+                <div className="flex flex-row items-center gap-2 p-0 hover:cursor-pointer">
+                  <div className="flex flex-row items-center gap-1.5 w-[61px] h-[27px] p-0">
+                    <EtherIcon />
+                    <div className="font-midium text-base leading-[27px] text-[#191B23]">
+                      ETH
+                    </div>
                   </div>
+                  <Vector />
                 </div>
-                <Vector />
+                <input
+                  type="number"
+                  onChange={handleCollateral}
+                  className={`Pretendard font-medium text-xl leading-6 text-[#191B23] text-right focus:outline-0 ${
+                    collateralValue === 0 && "text-[#E0E0E0]"
+                  }`}
+                />
               </div>
-              <input
-                type="number"
-                onChange={handleCollateral}
-                className={`Pretendard font-medium text-xl leading-6 text-[#191B23] text-right focus:outline-0 ${
-                  collateralValue === 0 && "text-[#E0E0E0]"
-                }`}
-              />
             </div>
           </div>
+        </div>
+        <div className="mt-11">
+          <button className="bg-[#2864ff] w-full h-14 rounded-lg left-0 top-0 font-normal text-base leading-6 text-white">
+            Make a list to sell
+          </button>
         </div>
       </div>
     </div>
