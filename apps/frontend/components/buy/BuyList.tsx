cb71270fddb1def84138fcbf99fe63c7905c3357
hunhunseunghun <hunhunseunghun@gmail.com>
2023-05-02 13:18:43 +0900

    adopted scroll

--- apps/frontend/components/buy/BuyList.tsx
+++ apps/frontend/components/buy/BuyList.tsx
@@ -51,7 +51,7 @@ const BuyList = () => {
         <span>2023.03.25 16:00</span>
       </td>
       <td className="align-middle text-center p-0">
-        <button className="bg-omi-blue w-[79px] h-8 rounded-[14px]">buy</button>
+        <button className="bg-omi-blue w-[79px] h-8 rounded-[14px]">Buy</button>
       </td>
     </tr>
   );
@@ -66,19 +66,21 @@ const BuyList = () => {
         </div>
       </header>
       <body className="mt-5">
-        <table className="w-[884px] Pretendard text-[#FFFFFF] font-normal text-[15px] leading-[18px]">
-          <thead className="h-10 Pretendard text-[#FFFFFF]  font-normal text-[15px] leading-[18px]">
-            <tr className="text-left ">
-              <th className="pl-3 font-normal p-0">All</th>
-              <th className="font-normal p-0">Price</th>
-              <th className="font-normal p-0">Collateral</th>
-              <th className="font-normal p-0">Time</th>
-              <th className="font-normal text-center p-0">Transaction</th>
+        <table className="block w-[884px] Pretendard text-[#FFFFFF] font-normal text-[15px] leading-[18px]">
+          <thead className="h-10 Pretendard text-[#FFFFFF]  font-normal text-[15px] leading-[18px] bg-[#22252E]">
+            <tr className="text-left">
+              <th className="pl-3 w-[170px] font-normal p-0">All</th>
+              <th className="w-[170px] font-normal p-0">Price</th>
+              <th className="w-[170px] font-normal p-0">Collateral</th>
+              <th className="w-[170px] font-normal p-0">Time</th>
+              <th className="w-[170px] font-normal text-center p-0">
+                Transaction
+              </th>
             </tr>
           </thead>
-          <tbody className="max-h-594px font-normal overflow-y-scroll">
-            <tr className="h-[66px]">
-              <td className="align-middle">
+          <tbody className="block w-full max-h-[594px] font-normal overflow-y-scroll">
+            <tr className="h-[66px] w-ful">
+              <td className="align-middle w-[170px]">
                 <div className="flex items-center gap-2 ml-3 p-0">
                   <div>1.</div>
                   <EtherIcon />
@@ -93,14 +95,14 @@ const BuyList = () => {
                   </div>
                 </div>
               </td>
-              <td>
+              <td className="w-[170px]">
                 <div className="flex flex-row items-center gap-1.5 p-0">
                   {" "}
                   <EtherIcon />
                   <span>1519.00</span>
                 </div>
               </td>
-              <td className="align-middle gap-2 p-0">
+              <td className="w-[170px] align-middle gap-2 p-0">
                 <div className="flex items-center gap-2 p-0">
                   <EtherIcon />
                   <div className="flex flex-col items-start gap-0.5 h-9 p-0">
@@ -114,26 +116,16 @@ const BuyList = () => {
                   </div>
                 </div>
               </td>
-              <td className="mr-3">
+              <td className="w-[170px] mr-3">
                 <span>2023.03.25 16:00</span>
               </td>
-              <td className="align-middle text-center p-0">
+              <td className="w-[170px] align-middle text-center p-0">
                 <button className="bg-omi-blue w-[79px] h-8 rounded-[14px]">
-                  buy
+                  Buy
                 </button>
               </td>
             </tr>
 
-            {tags}
-            {tags}
-            {tags}
-            {tags}
-            {tags}
-            {tags}
-            {tags}
-            {tags}
-            {tags}
-            {tags}
             {tags}
             {tags}
             {tags}
