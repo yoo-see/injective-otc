e5e360a7dfbf7332c7aa2fe72f1d26c2ab331f49
유지은 <wlwl0219@humaxparcs.com>
2023-05-04 16:21:50 +0900

    update icon

--- apps/frontend/public/icon/index.ts
+++ apps/frontend/public/icon/index.ts
@@ -1,18 +1,30 @@
+import IconAlarmClockRed from "./svg/alarm-clock-red.svg";
+import IconAlarmClock from "./svg/alarm-clock.svg";
 import IconAlertCircle from "./svg/alert-circle.svg";
 import IconArrowLeftRightWhite from "./svg/arrow-left-right-white.svg";
 import IconArrowLeftRight from "./svg/arrow-left-right.svg";
+import IconArrowRightCircle from "./svg/arrow-right-circle.svg";
 import IconBell from "./svg/bell.svg";
+import IconCheckCheck from "./svg/check-check.svg";
 import IconChevronCircleLeft from "./svg/chevron-circle-left.svg";
 import IconChevronLeft from "./svg/chevron-left.svg";
+import IconCloseRed from "./svg/close-red.svg";
 import IconClose from "./svg/close.svg";
 import IconEther from "./svg/ether.svg";
 import IconDashboard from "./svg/layout-dashboard.svg";
+import IconMoreHorizontal from "./svg/more-horizontal.svg";
 import IconLogo from "./svg/omi-logo.svg";
 import IconSearch from "./svg/search.svg";
 import IconUsdc from "./svg/usdc.svg";
 import IconVector from "./svg/vector.svg";
 
 export const SvgIcon = {
+  AlarmClockRed: IconAlarmClockRed,
+  AlarmClock: IconAlarmClock,
+  ArrowRightCircle: IconArrowRightCircle,
+  CheckCheck: IconCheckCheck,
+  CloseRed: IconCloseRed,
+  MoreHorizontal: IconMoreHorizontal,
   Logo: IconLogo,
   Search: IconSearch,
   Bell: IconBell,
